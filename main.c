da65ae3dcad29b653b9b0355dfbb8cdbd627fef2
joaopedrodominguesb-dotcom <joaopedrodominguesb@gmail.com>
2026-03-17 21:45:20 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,29 +1,18 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
-int main() {
-    float num1, num2, resultado;
+int main()
+{
+    float s1, s2, media;
 
-    printf("Digite o primeiro numero: ");
-    scanf("%f", &num1);
+    printf("Digite o valor do sensor 1: ");
+    scanf("%f", &s1);
 
-    printf("Digite o segundo numero: ");
-    scanf("%f", &num2);
+    printf("Digite o valor do sensor 2: ");
+    scanf("%f", &s2);
 
-    if (num2 == 0) {
-        printf("Erro: divisao por zero nao e permitida.\n");
-    } else {
-        resultado = num1 / num2;
-        printf("Resultado da divisao: %.2f\n", resultado);
-    }
+    media = (s1 + s2) / 2;
+
+    printf("A media eh: %.2f\n", media);
 
     return 0;
 }
-
